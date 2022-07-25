a25dda11dcfb1b76cdd0f4e35b1e934cbb272646
Todd Evans <evansmtodd@gmail.com>
2022-07-25 16:19:51 +1000

    Added card images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import './App.css';
 
+const cardimg = [
+  { "src": "../public/img/croc.jpg" },
+  { "src": "../public/img/roo.jpg" },
+  { "src": "../public/img/kookaburra.jpg" },
+  { "src": "../public/img/possum.jpg" },
+  { "src": "../public/img/emu.jpg" },
+  { "src": "../public/img/koala.jpg" },
+];
+
 function App() {
   return (
     <div className="App">
